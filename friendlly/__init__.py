f64e1c99ebc22e0517877818ce2020abe95ea4f5
Alexey <alexey.zaytsev@gmail.com>
2024-07-28 03:41:33 -0500

    config and vscode support

--- friendlly/__init__.py
+++ friendlly/__init__.py
@@ -2,12 +2,13 @@ __version__ = "0.1.1"
 
 from .magic_cell import fr_cell
 from .magic_line import fr_line
-from .utils.nbclassic import patch_kernel
+from .utils.notebook import nbclassic_patch_kernel, detect_environment
 from IPython.display import clear_output
 
 def load_ipython_extension(ipython):
-    patch_kernel()
-    clear_output()
+    if detect_environment() == "nbclassic":
+        nbclassic_patch_kernel()
+        clear_output()
 
     ipython.register_magic_function(fr_cell, "cell", magic_name="fr")
     ipython.register_magic_function(fr_line, "line", magic_name="fr")
